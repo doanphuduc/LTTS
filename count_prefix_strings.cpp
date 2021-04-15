21333f1e2d39df249577b27d584d3378d5aaf9b3
doanphuduc <doanphuducqh@gmail.com>
2021-04-15 10:09:13 +0700

    count number of prefix strings

---
+++ count_prefix_strings.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map <pair <long long, int>, int> cnt;
+
+const int B = 1e5 + 7;
+const int BASE = 1e9 + 13;
+
+// pair <long long, int> chứa hash 2 base, long long là hash tràn, int là hash mod BASE
+
+pair <long long, int> Hash(string &s) {
+    pair <long long, int> h;
+    for (char c: s) {
+        h.first = h.first * B + c - 'a' + 1; // + 1 để tránh trường hợp hash của aa = hash của a.
+        h.second = (h.second * B + c - 'a' + 1) % BASE;
+    } 
+    return h;
+}
+
+int main() {
+    int n; cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        pair <long long, int> h = {0, 0};
+        string s; cin >> s;
+        string r;
+        for (char c: s) {
+            r += c;
+            cnt[Hash(r)]++;
+        }
+    }
+    int q; cin >> q;
+    while (q--) {
+        string t; cin >> t;
+        cout << cnt[Hash(t)] << '\n';
+    }
+    return 0;
+}
