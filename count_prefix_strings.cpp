bb1095917379aba7253aafa96839128ae597eef1
doanphuducqh@gmail.com <doanphuducqh@gmail.com>
2021-04-21 23:26:14 +0700

    fix bug hash 2 based, just hash one based

--- count_prefix_strings.cpp
+++ count_prefix_strings.cpp
@@ -2,32 +2,23 @@
 
 using namespace std;
 
-map <pair <long long, int>, int> cnt;
+map <long long, int> cnt;
 
 const int B = 1e5 + 7;
-const int BASE = 1e9 + 13;
 
-// pair <long long, int> chứa hash 2 base, long long là hash tràn, int là hash mod BASE
-
-pair <long long, int> Hash(string &s) {
-    pair <long long, int> h;
-    for (char c: s) {
-        h.first = h.first * B + c - 'a' + 1; // + 1 để tránh trường hợp hash của aa = hash của a.
-        h.second = (h.second * B + c - 'a' + 1) % BASE;
-    } 
+long long Hash(string &s) {
+    long long h = 0;
+    for (char c: s) h = h * B + c;
     return h;
 }
 
 int main() {
     int n; cin >> n;
     for (int i = 1; i <= n; ++i) {
-        pair <long long, int> h = {0, 0};
         string s; cin >> s;
-        string r;
-        pair <long long, int> h;
+        long long h = 0;
         for (char c: s) {
-            h.first = h.first * B + c - 'a' + 1; // + 1 để tránh trường hợp hash của aa = hash của a.
-            h.second = (h.second * B + c - 'a' + 1) % BASE;
+            h = h * B + c;
             cnt[h]++;
         }
     }
