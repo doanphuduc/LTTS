0e9a9978e69cbb1d9714aa549fed204989ce98b9
doanphuducqh@gmail.com <doanphuducqh@gmail.com>
2021-04-22 10:48:30 +0700

    fixed bug

--- max_square_full_one_N^2.cpp
+++ max_square_full_one_N^2.cpp
@@ -16,8 +16,8 @@ int main() {
         for (int i = 1; i <= n; ++i)
             for (int j = 1; j <= m; ++j) cin >> a[i][j];
         int ans = 0;
-        for (int i = 2; i <= n; ++i) {
-            for (int j = 2; j <= m; ++j) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
                 if (a[i][j] == 1) a[i][j] = min({a[i - 1][j], a[i - 1][j - 1], a[i][j - 1]}) + 1;
                 else a[i][j] = 0;
                 ans = max(ans, a[i][j]);
