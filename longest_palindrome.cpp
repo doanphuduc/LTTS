c15d73790dca50fe55ed8a2906f8706058299e6a
doanphuducqh@gmail.com <doanphuducqh@gmail.com>
2021-04-22 10:34:40 +0700

    convert overflow hash into modular hash, BA = 1e5 + 3, BASE = 1e9 + 21 is prime

--- longest_palindrome.cpp
+++ longest_palindrome.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 const int N = 2e5 + 10;
 const int B = 1e5 + 3;
+const int BASE = 1e9 + 21;
 
 // Tested: https://oj.vnoi.info/problem/paliny
 
@@ -15,28 +16,26 @@ long long pw[N];
 long long H[2][N];
 
 long long Hash(int k, int l, int r) {
-    return H[k][r] - H[k][l - 1] * pw[r - l + 1];
+    return (H[k][r] - H[k][l - 1] * pw[r - l + 1] + 1LL * BASE * BASE) % BASE;
 }
 
 bool Palin(int l, int r) {
     return Hash(0, l, r) == Hash(1, n - r + 1, n - l + 1);
 
 }
- 
+
 int main() {
     cin >> n;
     string T; cin >> T;
-    n = n * 2 + 1; 
-    for (int i = 1; i <= n; ++i) 
+    n = n * 2 + 1; // remember doubling memory when doubling string
+    for (int i = 1; i <= n; ++i)
         S[i] = (i & 1) ? '#' : T[i / 2 - 1]; // -1 do string T 0-indexed
     pw[0] = 1;
     for (int i = 1; i <= n; ++i) {
-        pw[i] = pw[i - 1] * B;
-        H[0][i] = H[0][i - 1] * B + S[i];
-        H[1][i] = H[1][i - 1] * B + S[n - i + 1];
-        // cout << S[i];
+        pw[i] = pw[i - 1] * B % BASE;
+        H[0][i] = (H[0][i - 1] * B + S[i]) % BASE;
+        H[1][i] = (H[1][i - 1] * B + S[n - i + 1]) % BASE;
     }
-    // cout << '\n';
     int ans = 0;
     for (int i = 1; i <= n; ++i) {
         int l = 1, r = min(i, n - i + 1), f = 0;
