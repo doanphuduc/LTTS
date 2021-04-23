7294f3235eedecfa34adb5d9984b145c03cdd860
doanphuducqh@gmail.com <doanphuducqh@gmail.com>
2021-04-23 23:32:58 +0700

    Palindrome O(N)

--- longest_palindrome.cpp
+++ longest_palindrome.cpp
@@ -1,54 +1,51 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
-const int N = 2e5 + 10;
-const int B = 1e5 + 3;
-const int BASE = 1e9 + 21;
-
-// Tested: https://oj.vnoi.info/problem/paliny
-
+ 
+const int N = 1e6;
+const long long B = 1e5+3;
+const long long BASE = 1e9 + 21;
+ 
 int n;
-
-char S[N];
-
-long long pw[N];
-long long H[2][N];
-
-long long Hash(int k, int l, int r) {
-    return (H[k][r] - H[k][l - 1] * pw[r - l + 1] + 1LL * BASE * BASE) % BASE;
+string s;
+char a[N];
+long long H[2][N], pw[N];
+ 
+long long Hash(int k, int l, int r)
+{
+	return (H[k][r] - H[k][l-1] * pw[r - l + 1] + BASE * BASE) % BASE;
 }
-
-bool Palin(int l, int r) {
-    return Hash(0, l, r) == Hash(1, n - r + 1, n - l + 1);
-
+ 
+bool Palind(int i, int d)
+{
+	int l = i - d + 1;
+	int r = i + d - 1;
+	return Hash(0, l, r) == Hash(1, n - r + 1, n - l + 1);
 }
-
-int main() {
-    cin >> n;
-    string T; cin >> T;
-    n = n * 2 + 1; // remember doubling memory when doubling string
-    for (int i = 1; i <= n; ++i)
-        S[i] = (i & 1) ? '#' : T[i / 2 - 1]; // -1 do string T 0-indexed
-    pw[0] = 1;
-    for (int i = 1; i <= n; ++i) {
-        pw[i] = pw[i - 1] * B % BASE;
-        H[0][i] = (H[0][i - 1] * B + S[i]) % BASE;
-        H[1][i] = (H[1][i - 1] * B + S[n - i + 1]) % BASE;
-    }
-    int ans = 0;
-    for (int i = 1; i <= n; ++i) {
-        int l = 1, r = min(i, n - i + 1), f = 0;
-        while (l <= r) {
-            int m = (l + r) >> 1;
-            if (Palin(i - m + 1, i + m - 1)) {
-                f = m;
-                l = m + 1;
-            } else r = m - 1;
-        }
-
-        ans = max(ans, f);
-    }
-    cout << (2 * ans - 1) / 2;
-    return 0;
+ 
+int main()
+{
+	cin >> n;
+	cin >> s;
+	n = 2 * n + 1;
+	for(int i = 1; i <= n; ++i)
+	{
+		a[i] = i & 1 ? '#' : s[i / 2 - 1];
+	}
+ 
+	pw[0] = 1;
+	for(int i = 1; i <= n; ++i) pw[i] = pw[i-1] * B % BASE;
+ 
+	for(int i = 1; i <= n; ++i)
+	{
+		H[0][i] = (H[0][i-1] * B + a[i]) % BASE;
+		H[1][i] = (H[1][i-1] * B + a[n-i+1]) % BASE;
+	}
+ 
+	int res = 1;
+	for(int i = 1; i <= n; ++i)
+	{
+		while(res + 1 <= min(i, n - i + 1) && Palind(i, res + 1)) res++;
+	}
+	cout << (2 * res - 1) / 2 << '\n';
 }
